c1fafe8b87583da70b8b93c372922366caaf0aa0
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-21 15:36:03 +0900

    Feat: tag list 조회 시 글 개수 포함

--- src/main/java/com/minjaedev/blogback/dto/user/TagResponseDto.java
+++ src/main/java/com/minjaedev/blogback/dto/user/TagResponseDto.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 
 @Getter
 public class TagResponseDto {
-    private Long tagId;
-    private String name;
+    private final Long tagId;
+    private final String name;
+    private final int postCount;
+
 
     public TagResponseDto(Tag tag) {
         this.tagId = tag.getId();
         this.name = tag.getName();
+        this.postCount = tag.getPosts().size();
     }
 }
